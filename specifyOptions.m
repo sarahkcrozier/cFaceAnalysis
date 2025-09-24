53db50d126b9689ed39fa6b7ce39df73f0c4f054
yamaan <yamaan@yamaan-2.newcastle.edu.au>
2025-09-24 16:43:55 +1000

    Use peak HR from each trial response window instead of mean. Average all peaks = HR.incongruentAverage

--- specifyOptions.m
+++ specifyOptions.m
@@ -10,8 +10,8 @@ options.study.tasks = {'cFace','FF1','HBD'};
 
 options.paths.workingDir = pwd;
 options.paths.analysis   = ['/Users/yamaan/Projects/',options.study.acronym,filesep,options.study.tasks{1},'Analysis',filesep];
-% options.paths.data     = ['/Volumes/PCNS/Data/Data_raw/together'];
- options.paths.data       = ['/Users/yamaan/Projects/',options.study.acronym,filesep,'Data/RawData',filesep]; %temp local data path for testing
+options.paths.data     = ['/Volumes/PCNS/Data/Data_raw/together'];
+% options.paths.data       = ['/Users/yamaan/Projects/',options.study.acronym,filesep,'Data/RawData',filesep]; %temp local data path for testing
 options.paths.DBExport   = ['/Users/yamaan/Projects/',options.study.acronym,filesep,'Data/REDCapExport',filesep];
 % options.paths.DBExport   = ['/Users/yamaan/Projects/',options.study.acronym,filesep,'Data/REDCapExportTEST',filesep]; %temp DBExport file that only includes two participant, for testing
 options.paths.HBDExport   = ['/Users/yamaan/Projects/',options.study.acronym,filesep,'Data/HBDAnalysis',filesep];
